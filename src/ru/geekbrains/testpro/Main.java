5a3e7049f34337b0d9c09b3bf186abb77f12cf1d
arsvlad <arsvlad2020@gmail.com>
2020-08-22 12:51:50 +0300

    Test repository from IDEA

---
+++ src/ru/geekbrains/testpro/Main.java
@@ -0,0 +1,9 @@
+package ru.geekbrains.testpro;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Test Message");
+    }
+}
